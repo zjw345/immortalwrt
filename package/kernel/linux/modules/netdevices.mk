3d10bebcd1f05f3f00375373a3311a8e5bacd516
lkiuyu <1340320610@qq.com>
2024-09-25 20:40:43 +0800

    use kernel 6.6 by default

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1951,7 +1951,7 @@ $(eval $(call KernelPackage,mhi-wwan-mbim))
 define KernelPackage/rpmsg-wwan-ctrl
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=RPMSG WWAN Control
-  DEPENDS:=@LINUX_6_1 +kmod-wwan
+  DEPENDS:=@LINUX_6_1||LINUX_6_6 +kmod-wwan
   KCONFIG:=CONFIG_RPMSG_WWAN_CTRL
   FILES:=$(LINUX_DIR)/drivers/net/wwan/rpmsg_wwan_ctrl.ko
   AUTOLOAD:=$(call AutoProbe,rpmsg_wwan_ctrl)
